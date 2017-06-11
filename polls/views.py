96d2aff0e6cabdc5b276ec2e6d1ad4cd17567c35
Omkar Panherkar <omipanherkar@gmail.com>
2017-06-11 23:24:01 +0530

    My Django Girls app, Second commit

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def index(request):
     return HttpResponse("Hello Omkar")
 
 def create(request):
     return HttpResponse("Creatsadasdaasde Omkar")
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'polls/post_list.html', {'posts': posts})
